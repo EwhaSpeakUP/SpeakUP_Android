8e1b241727d046a224efa688f7e9d5f7b69bcead
jionchu <fn0809@gmail.com>
2020-09-08 13:36:09 +0900

    init commit

---
+++ app/src/main/java/com/jionchu/speakup/config/XAccessTokenInterceptor.java
@@ -0,0 +1,26 @@
+package com.jionchu.speakup.config;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static com.jionchu.speakup.src.ApplicationClass.X_ACCESS_TOKEN;
+import static com.jionchu.speakup.src.ApplicationClass.sSharedPreferences;
+
+public class XAccessTokenInterceptor implements Interceptor {
+
+    @Override
+    @NonNull
+    public Response intercept(@NonNull final Chain chain) throws IOException {
+        final Request.Builder builder = chain.request().newBuilder();
+        final String jwtToken = sSharedPreferences.getString(X_ACCESS_TOKEN, null);
+        if (jwtToken != null) {
+            builder.addHeader("X-ACCESS-TOKEN", jwtToken);
+        }
+        return chain.proceed(builder.build());
+    }
+}
---
+++ app/src/main/java/com/jionchu/speakup/src/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jionchu.speakup.src.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jionchu.speakup.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
